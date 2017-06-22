cd4636fa5c5d1803e4257a19166de3052de60db5
Jerhone <fjcamillo.dev@gmail.com>
2017-06-22 20:13:35 +0800

    Added gradient descent

--- GradientDescent.py
+++ GradientDescent.py
@@ -8,5 +8,12 @@ def model(type, x, theta):
         hypothesis = reduce(lambda x, y: np.add(x, y), line)
         return hypothesis
 
+def gradient_descent(type, learning_rate, hypothesis, x, y, theta):
+    if type == 'linear':
+        cost = np.multiply(np.subtract(hypothesis, y), x)
+        train = np.subtract(theta, np.multiply(cost, learning_rate))
+        return train
+        
+
 if __name__ == '__main__':
     main()
